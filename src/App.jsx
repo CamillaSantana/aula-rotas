9d85051f5e7e4d0b2cc817f8e7d9cf00dd5d497b
CamillaSantana <RM99491@fiap.com.br>
2023-09-14 23:06:18 -0300

    listaProdutos.js criado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Rodape from './components/Rodape'
+import Menu from './components/Menu'
 
 function App() {
   
   return(
     <>
+      <Menu/>
       <Outlet/>
+      <Rodape/>
     </>
   )
 
