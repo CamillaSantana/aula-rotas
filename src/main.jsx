d8df86cb563bed70daa61c9d01eeb4dc006f17be
CamillaSantana <RM99491@fiap.com.br>
2023-09-14 22:39:23 -0300

    rotas configuradas

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,29 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from  './routes/Home/index.jsx'
 import Produtos from  './routes/Produtos/index.jsx'
 import Error from  './routes/Error/index.jsx'
-import EditarProdutos from  './routes/EditarProdutos/index.jsx'
+import EditarProduto from "./routes/EditarProduto/index.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProduto/>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
